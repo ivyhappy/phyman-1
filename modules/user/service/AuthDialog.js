e8eb2edfbfb3c60887703422414ad41cf8bec29d
ivy.happy <ivy.java@gmail.com>
2016-02-24 22:45:46 +0800

    Revert "Revert "some changes""
    This reverts commit f73867aa4dd9e7953cb4849b5038e5d44b6778c7.

--- modules/user/service/AuthDialog.js
+++ modules/user/service/AuthDialog.js
@@ -1,27 +1,35 @@
 angular.module('phyman.user')
-    .factory('AuthDialog', ['$mdDialog', function($mdDialog){
+    .factory('AuthDialog', ['$mdDialog', '$state',function($mdDialog,$state){
         return {
             cancel: function() {
-                $mdDialog.cancel();
+                $mdDialog.hide();
             },
             hide: function() {
                 $mdDialog.hide();
             },
             showLoginDialog: function(ev) {
+                $mdDialog.hide();
+                $state.go("lg",null,{
+                	reload:true
+                });
                 $mdDialog.show({
                     controller: 'LoginCtrl',
-                    templateUrl: 'modules/user/views/login.html',
+                    templateUrl: './Background/Home/phyman-1/modules/user/views/login.html',
                     parent: angular.element(document.body),
                     targetEvent: ev,
                     clickOutsideToClose: false
                 });
             },
             showRegisterDialog: function(ev) {
+                $mdDialog.hide();
+                $state.go("login",null,{
+                	reload:true
+                });
                 $mdDialog.show({
                     controller: 'RegisterCtrl',
-                    templateUrl: 'modules/user/views/register.html',
+                    templateUrl: './Background/Home/phyman-1/modules/user/views/register.html',
+                    reload:true,
                     parent: angular.element(document.body),
-                    targetEvent: ev,
                     clickOutsideToClose: false
                 });
             }
